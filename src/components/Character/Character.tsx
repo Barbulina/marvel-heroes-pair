945ca87f9910ae8c65b41c867812dbdd35879abc
Emilio Fernández <barbulina@gmail.com>
2021-11-03 15:39:51 +0100

    add button to remove and add

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,14 +1,33 @@
-import React from "react";
 import { Button, Card } from "antd";
 import { CharacterModel } from "./character.model";
-import { PlusCircleOutlined } from "@ant-design/icons";
+import {
+  PlusCircleOutlined,
+  MinusCircleOutlined,
+  InfoCircleOutlined,
+} from "@ant-design/icons";
 
 const { Meta } = Card;
 
-export function Character({ character }: { character: CharacterModel }) {
+export function Character({
+  character,
+}: {
+  character: CharacterModel;
+}): JSX.Element {
   const aspecRatio = "portrait_xlarge";
   const image: string = `${character.thumbnail.path}/${aspecRatio}.${character.thumbnail.extension}`;
   const cover = <img alt="example" src={image} />;
+
+  // TODO  move to click in card
+  const infoButton = (
+    <>
+      <Button
+        icon={<InfoCircleOutlined />}
+        onClick={() => handlerGoToInfoCharacter(character)}
+      >
+        Info
+      </Button>
+    </>
+  );
   const addButton = (
     <>
       <Button
@@ -16,20 +35,38 @@ export function Character({ character }: { character: CharacterModel }) {
         icon={<PlusCircleOutlined />}
         onClick={() => handlerAddCharacter(character)}
       >
-        Add user
+        Add
       </Button>
     </>
   );
-  const handlerAddCharacter = (character: CharacterModel) => {
+  const removeBotton = (
+    <>
+      <Button
+        danger
+        type="primary"
+        icon={<MinusCircleOutlined />}
+        onClick={() => handlerRemoveCharacter(character)}
+      >
+        Remove
+      </Button>
+    </>
+  );
+  const handlerGoToInfoCharacter = (character: CharacterModel): void => {
+    console.log("Character info of", character);
+  };
+  const handlerAddCharacter = (character: CharacterModel): void => {
     console.log("add ", character);
   };
-  const actions = [addButton];
+  const handlerRemoveCharacter = (character: CharacterModel): void => {
+    console.log("remove", character);
+  };
+  const actions: JSX.Element[] = [infoButton, addButton, removeBotton];
 
   return (
-    <React.Fragment>
+    <>
       <Card hoverable style={{ width: "100%" }} cover={cover} actions={actions}>
         <Meta title={character.name} />
       </Card>
-    </React.Fragment>
+    </>
   );
 }
